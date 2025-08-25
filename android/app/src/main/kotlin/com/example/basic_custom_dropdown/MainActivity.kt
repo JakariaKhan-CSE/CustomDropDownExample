60ba6866f389a8d1bf31302469e94a3814a2eaa5
Md. Jakaria Ibna Azam Khan <mdjakariaibnaazamkhan@gmail.com>
2025-08-25 19:17:14 +0600

    brand new project.

---
+++ android/app/src/main/kotlin/com/example/basic_custom_dropdown/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.basic_custom_dropdown
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/CustomDropdownExample.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class CustomDropDownExample extends StatefulWidget {
+  const CustomDropDownExample({super.key});
+
+  @override
+  State<CustomDropDownExample> createState() => _CustomDropDownExampleState();
+}
+
+class _CustomDropDownExampleState extends State<CustomDropDownExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
